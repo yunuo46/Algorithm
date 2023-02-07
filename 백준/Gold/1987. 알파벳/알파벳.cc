2766f474b8b7a3bf0507b32dab7e3f3cf83d025e
yunuo46 <83938394+yunuo46@users.noreply.github.com>
2023-02-07 17:49:21 +0900

    [Gold IV] Title: 알파벳, Time: 424 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,10 +1,5 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <vector>
-#include <map>
-#include <stack>
-#include <queue>
+#include <bits/stdc++.h>
+#define int long long
 #define endl "\n"
 using namespace std;
 
@@ -16,31 +11,32 @@ int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
 
 void dfs(int x, int y, int dist) {
-    ans = max(dist, ans);
-    int now = maps[x][y] - 65;
-    for (int i = 0; i < 4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            int next = maps[nx][ny] - 65;
-            if (nx >= 0 && nx < r && ny >= 0 && ny < c && !alpha[next]) {
-                alpha[next] = true;
-                dfs(nx, ny, dist+1);
-                alpha[next] = false;
-            }
-    }
+	ans = max(dist, ans);
+	int now = maps[x][y] - 65;
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		int next = maps[nx][ny] - 65;
+		if (nx >= 0 && nx < r && ny >= 0 && ny < c && !alpha[next]) {
+			alpha[next] = true;
+			dfs(nx, ny, dist + 1);
+			alpha[next] = false;
+		}
+	}
 }
 
-int main()
+int32_t main()
 {
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    cin >> r >> c;
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            cin >> num;
-            maps[i][j] = (int)num;
-        }
-    }
-    alpha[maps[0][0] - 65] = true;
-    dfs(0, 0, 1);
-    cout << ans;
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+	cin >> r >> c;
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cin >> num;
+			maps[i][j] = (int)num;
+		}
+	}
+	alpha[maps[0][0] - 65] = true;
+	dfs(0, 0, 1);
+	cout << ans;
 }
