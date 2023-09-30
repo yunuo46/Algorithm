ec48843a90a5fbf80df545b3e4623289476b97d2
yunuo46 <83938394+yunuo46@users.noreply.github.com>
2023-10-01 00:44:24 +0900

    [Platinum IV] Title: 축구 전술, Time: 516 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Platinum/3977. 축구 전술/축구 전술.cc
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int v, e, dfn;
+stack<int> s;
+vector<int> Edges[100001];
+bool finished[100001];
+vector<vector<int>> SCC;
+int Dfn[100001];
+int pid[100001];
+bool isRoot[100001];
+
+int dfs(int x) {
+	Dfn[x] = ++dfn;
+	s.push(x);
+	int p = Dfn[x];
+	for (int i = 0; i < Edges[x].size(); i++) {
+		int y = Edges[x][i];
+		if (Dfn[y] == 0) p = min(p, dfs(y)); // 방문X
+		else if (!finished[y]) p = min(p, Dfn[y]); // 처리 중
+	}
+	if (p == Dfn[x]) {
+		vector<int> scc;
+		while (true) {
+			int t = s.top();
+			s.pop();
+			scc.push_back(t);
+			finished[t] = true;
+			pid[t] = x; // 그룹 번호 저장(pid)
+			if (t == x)break;
+		}
+		sort(scc.begin(), scc.end());
+		SCC.push_back(scc);
+	}
+	return p;
+}
+
+int main() {
+	int t, i, x, y, ans;
+	cin >> t;
+	while (t--) {
+		cin >> v >> e;
+
+		// 초기화
+		memset(isRoot, true, sizeof(isRoot));
+		memset(finished, false, sizeof(finished));
+		memset(Dfn, 0, sizeof(Dfn));
+		memset(pid, 0, sizeof(pid));
+		dfn = 0;
+		ans = 0;
+		SCC.clear();
+		for (int i = 0; i < 100001; i++) {
+			Edges[i].clear();
+		}
+
+		for (i = 0; i < e; i++) {
+			cin >> x >> y;
+			Edges[x].push_back(y);
+		}
+		for (i = 0; i < v; i++) {
+			if (Dfn[i] == 0) dfs(i);
+		}
+		for (i = 0; i < v; i++) {
+			for (int j = 0; j < Edges[i].size(); j++) {
+				int y = Edges[i][j];
+				if (pid[i] != pid[y]) isRoot[pid[y]] = false;
+			}
+		}
+		for (i = SCC.size()-1; i >= 0; i--) {
+			if (isRoot[pid[SCC[i][0]]]) ans++;
+		}
+		if (ans != 1)cout << "Confused" << endl;
+		else {
+			for (int& element : SCC[SCC.size() - 1]) cout << element << endl;
+		}
+		cout << endl;
+	}
+	return 0;
+}
