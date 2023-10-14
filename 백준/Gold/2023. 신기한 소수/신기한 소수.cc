e00039fd9221263c8aee7869e3a4bf59a13d8d5c
yunuo46 <83938394+yunuo46@users.noreply.github.com>
2023-10-14 11:40:36 +0900

    [Gold V] Title: 신기한 소수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int n;
+vector<long long> sol;
+
+int check(long long n) {
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) return 0;
+	}
+	return 1;
+}
+
+void dfs(int now, long long num) {
+    int flag;
+    long long temp;
+
+    if (now == n) {
+        sol.push_back(num);
+        return;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        temp = num * 10;
+        temp += i;
+        flag = check(temp);
+        if (flag == 0)
+            continue;
+
+        dfs(now + 1, temp);
+    }
+}
+
+int main(void) {
+
+    cin >> n;
+
+    dfs(1, 2);
+    dfs(1, 3);
+    dfs(1, 5);
+    dfs(1, 7);
+
+    sort(sol.begin(), sol.end());
+
+    for (int i = 0; i < sol.size(); i++)
+        cout << sol[i] << endl;
+}
